f68c48f754a7c4b1cc535261a72c870412c09257
Ram Shiv <67915300+Ramshiv7@users.noreply.github.com>
2023-05-14 10:42:56 +0530

    Initial Code for Resume

---
+++ index.js
@@ -0,0 +1,17 @@
+
+count = 0 
+likedCount = document.getElementById("count")
+
+function increment() {
+    count = count + 1;
+    likedCount = document.getElementById("count")
+    likedCount.innerText = count
+    console.log("the button was clicked " + count + " times")
+}                                                                                           
+
+function savedCount() {
+    savedCount = count 
+    console.log("the Count Saved is " + savedCount)
+    
+}
+  
